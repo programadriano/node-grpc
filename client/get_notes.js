156278af1adf10b0980188b6496f90ea0b3cc50f
Thiago da Silva Adriano <tadriano.net@gmail.com>
2019-12-22 09:36:12 -0300

    finish post

--- client/get_notes.js
+++ client/get_notes.js
@@ -1,7 +1,7 @@
 const client = require('./client-grpc')
+
 client.list({}, (error, notes) => {
     if (!error) {
-        console.log('successfully fetch List notes')
         console.log(notes)
     } else {
         console.error(error)
